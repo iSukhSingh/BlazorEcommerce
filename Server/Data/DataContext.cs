77de4f49799e5f49631a9828a925e8c3dec0a0ec
Sukh Singh <69435459+iSukhSingh@users.noreply.github.com>
2023-02-20 13:31:49 -0500

    Add project files.

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 3451,
+                    Title = "NTN",
+                    Description = "Company",
+                    ImageUrl = "https://marvel-b1-cdn.bc0a.com/f00000000229351/ntnamericas.com/wp-content/uploads/2020/03/750031_2.png",
+                    Price = 39.99m
+                },
+            new Product
+            {
+                Id = 1435,
+                Title = "google",
+                Description = "Tech Company",
+                ImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                Price = 29.99m
+            }
+                );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
