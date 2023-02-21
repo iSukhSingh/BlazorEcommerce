c95d1b272c8b98028d99661534ba765c46712198
Sukh Singh <69435459+iSukhSingh@users.noreply.github.com>
2023-02-21 09:12:05 -0500

    added the API tp fetch Single item

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -7,7 +7,23 @@
         {
             _context = context;
         }
-        public async Task<ServiceResponse<List<Product>>> GetProductAsyncs()
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var reponse = new ServiceResponse<Product>();
+            var product = await _context.Products.FindAsync(productId);
+            if (product != null)
+            {
+                reponse.Success= false;
+                reponse.Message = "This item does not exist";
+            }
+            else
+            {
+                reponse.Data = product;
+            }
+            return reponse;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsyncs()
         {
             var reponse = new ServiceResponse<List<Product>>
             {
