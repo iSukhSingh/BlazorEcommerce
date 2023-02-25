5b4b6eb90799d33dafd01f7e6cec530ace2df782
Sukh Singh <69435459+iSukhSingh@users.noreply.github.com>
2023-02-24 20:54:39 -0500

    login backend

--- BlazorEcommerce/Server/Controllers/AuthController.cs
+++ BlazorEcommerce/Server/Controllers/AuthController.cs
@@ -8,27 +8,39 @@ namespace BlazorEcommerce.Server.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+
         public AuthController(IAuthService authService)
-        { 
+        {
             _authService = authService;
-
         }
-
 
         [HttpPost("register")]
         public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
         {
-            var response = await _authService.Register(new Shared.User
-            {
-                Email = request.Email
-            },
-            request.Password);
+            var response = await _authService.Register(
+                new User
+                {
+                    Email = request.Email
+                },
+                request.Password);
 
-
-            if(!response.Success)
+            if (!response.Success)
             {
                 return BadRequest(response);
             }
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLogin request)
+        {
+            var response = await _authService.Login(request.Email, request.Password);
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
             return Ok(response);
         }
     }
